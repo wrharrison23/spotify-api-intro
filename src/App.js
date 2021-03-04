6419524dbb1b5578d3ebe84d23ea9d16a3da655e
Wes <wrharrison23@gmail.com>
2021-03-04 16:46:25 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 
 import './App.css';
-import React from "react"
+import React, {useState, useEffect} from "react"
 import SpotifyWebApi from "spotify-web-api-js"
+import { useParams } from "react-router-dom";
+
 var Spotify = require("spotify-web-api-js");
-var s = new Spotify();
+
 
 const spotifyApi = new SpotifyWebApi()
 // ** read README at  https://github.com/jmperez/spotify-web-api-js
-// ** Next: store access token in object
 
+export const App = () => {
+  const [auth, setAuth] = useState({})
+  const [tracks, setTracks] = useState({})
 
-class App extends React.Component {
-  constructor() {
-    super();
-    const params = this.getHashParams();
-    console.log(params);
-    const token = params.access_token;
-    console.log(token);
-    this.state = {
-      loggedIn: token ? true : false,
-      token: token,
-      tracks: "",
-    };
-  }
-  getHashParams() {
+  const getHashParams = () => {
     var hashParams = {};
     var e,
       r = /([^&;=]+)=?([^&;]*)/g,
@@ -34,43 +25,60 @@ class App extends React.Component {
       e = r.exec(q);
     }
     return hashParams;
-  }
+  };
 
- 
+    useEffect(() => {
+      debugger;
+      if(!auth.token){
+        const params = getHashParams();
+        console.log(params);
+        const token = params.access_token;
+        console.log(token);
+        debugger;
+        setAuth(
+          {
+            loggedIn: token ? true : false,
+            token: token,
+          },
+          []
+      )
+      }
+      ;
+    });
 
-  getTopTracks = () => {
-     
-     
-    spotifyApi.setAccessToken(this.state.token);
-     
-     
+
+  const getTopTracks = () => {
+    spotifyApi.setAccessToken(auth.token); 
+   
     spotifyApi.getMyTopTracks(trackOptions)
+    
     .then((r) => {
-      this.setState({
+      debugger
+      setTracks({
         tracks: r.items[0].album.artists[0].name
       })
     })
   }
 
-  render() {
+  const trackOptions = {
+    time_range: "medium_term",
+    limit: 1,
+    offset: 1,
+  };
+  
     return (
       <>
       <div className="App">
         <a href="http://localhost:8888"> Login to Spotify </a>
-        <div>Top Tracks: {this.state.tracks}</div>
+        <div>Top Tracks: {tracks.tracks}</div>
       </div>
       <div className="getTracksDiv">
-        <button onClick={() => this.getTopTracks() }>Get Tracks</button>
+        <button onClick={() => getTopTracks()}>Get Tracks</button>
       </div>
       </>
     );
-  }
-}
+  
 
-const trackOptions = {
-  "time_range": "medium_term",
-  "limit": 1,
-  "offset": 1
-}
 
-export default App;
+
+}
